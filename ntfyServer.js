1466bc9255806e63f4243378d903b21a4c7c1a1d
Erorus <erorus@users.noreply.github.com>
2025-07-14 00:36:05 -0400

    add ntfy websocket support

--- ntfyServer.js
+++ ntfyServer.js
@@ -1,13 +1,14 @@
 const dateFormat = require('dateformat');
 const { createServer } = require('node:http');
-const WebSocketServer = require('ws');
+const { WebSocketServer } = require('ws');
 
 /**
  * @param {UpdateMessage[]} backlog
  */
 module.exports = function (backlog) {
     const HTTP_PORT = 8003;
-    const WSS_PORT = 8004;
+    const WS_PORT = 8004;
+    const LISTEN_HOST = undefined;
     const KEEPALIVE_INTERVAL = 45000;
 
     /**
@@ -33,10 +34,10 @@ module.exports = function (backlog) {
      * Ensures we have the required properties on a Ntfy message, then returns it as a json string.
      *
      * @param {Object} msg
-     * @param {string} action
+     * @param {string|null} action
      * @return {string}
      */
-    const encodeNtfyMessage = (msg, action) => {
+    const encodeNtfyMessage = (msg, action = null) => {
         msg.id ??= [...Array(12)].map(() =>
             "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
                 .charAt(Math.floor(Math.random() * 62))
@@ -149,52 +150,7 @@ module.exports = function (backlog) {
             write({'event': 'open'});
         }
 
-        // print old messages
-        if (backlog.length) {
-            let sinceTimestamp;
-            let sinceSequence;
-
-            if (since) {
-                const match = since.match(/^(\d+)(ms|s|m|h|d)$/);
-                if (match) {
-                    const [, amount, unit] = match;
-                    const n = parseInt(amount, 10);
-
-                    switch (unit) {
-                        case 'ms':
-                            sinceTimestamp = n;
-                        case 's':
-                            sinceTimestamp = n * 1000;
-                        case 'm':
-                            sinceTimestamp = n * 60 * 1000;
-                        case 'h':
-                            sinceTimestamp = n * 60 * 60 * 1000;
-                        case 'd':
-                            sinceTimestamp = n * 24 * 60 * 60 * 1000;
-                    }
-                } else
-                    if (/^\d+$/.test(since)) {
-                        sinceTimestamp = parseInt(since) * 1000;
-                    } else
-                        if (/^seq(\d+)$/.test(since)) {
-                            sinceSequence = parseInt(since.substring(3));
-                        }
-            }
-
-            {
-                let catchUp = [];
-                if (since === 'latest') {
-                    catchUp.push(backlog[backlog.length - 1]);
-                } else if (since != null) {
-                    catchUp = backlog.filter(message =>
-                        message.timestamp > (sinceTimestamp ?? 0) &&
-                        message.sequence > (sinceSequence ?? 0)
-                    );
-                }
-
-                catchUp.forEach(msg => write(compose(msg)));
-            }
-        }
+        handleSince(since, write);
 
         if (oneShot) {
             res.end();
@@ -203,7 +159,7 @@ module.exports = function (backlog) {
         }
 
         const keepaliveInterval = setInterval(() => void write({'event': 'keepalive'}), KEEPALIVE_INTERVAL);
-        clients.push({res, write});
+        clients.push({res, topic, write});
         res.on('close', () => {
             clearInterval(keepaliveInterval);
 
@@ -216,11 +172,114 @@ module.exports = function (backlog) {
 
     {
         const server = createServer(handleHTTPRequest);
-        server.listen(HTTP_PORT, undefined, () => logMsg(`Ntfy.sh HTTP server started on port ${HTTP_PORT}.`));
+        server.listen(HTTP_PORT, LISTEN_HOST, () => logMsg(`Ntfy.sh HTTP server started on port ${HTTP_PORT}.`));
     }
+    let wss;
+    {
+        const server = createServer();
+        wss = new WebSocketServer({ server });
+        wss.on('connection', (ws, req) => {
+            ws.on('ping', () => void ws.pong());
+            ws.on('pong', () => void (ws.isAlive = true));
+
+            const url = new URL(`http://localhost${req.url}`);
+            const match = url.pathname.match(/^\/([^\/,]+)\/ws$/);
+            if (!match) {
+                ws.close();
+                return;
+            }
+
+            const topic = match[1];
+            const write = msg => ws.send(encodeNtfyMessage({topic, ...msg}));
+            ws.ribbit = {topic, write};
+
+            const oneShot = getParam(req, ['poll', 'x-poll', 'po']) != null;
+            const since = getParam(req, ['since', 'x-since', 'si']);
+
+            if (!oneShot) {
+                write({'event': 'open'});
+            }
+
+            handleSince(since, write);
+
+            if (oneShot) {
+                ws.close();
+            }
+        });
+        const pingInterval = setInterval(() => {
+            wss.clients.forEach(ws => {
+                if (ws.isAlive === false) {
+                    return ws.terminate();
+                }
+
+                ws.isAlive = false;
+                ws.ping();
+            });
+        }, KEEPALIVE_INTERVAL);
+        wss.on('close', () => clearInterval(pingInterval));
+        server.listen(WS_PORT, LISTEN_HOST, () => logMsg(`Ntfy.sh WS server started on port ${WS_PORT}.`));
+    }
+
+    /**
+     * Prints the backlog to the connection as requested.
+     *
+     * @param {string|null} since
+     * @param {function} write
+     */
+    const handleSince = (since, write) => {
+        if (!backlog.length || since == null) {
+            return;
+        }
+
+        let sinceTimestamp;
+        let sinceSequence;
+
+        const match = since.match(/^(\d+)(ms|s|m|h|d)$/);
+        if (match) {
+            const [, amount, unit] = match;
+            const n = parseInt(amount, 10);
+
+            switch (unit) {
+                case 'ms':
+                    sinceTimestamp = n;
+                    break;
+                case 's':
+                    sinceTimestamp = n * 1000;
+                    break;
+                case 'm':
+                    sinceTimestamp = n * 60 * 1000;
+                    break;
+                case 'h':
+                    sinceTimestamp = n * 60 * 60 * 1000;
+                    break;
+                case 'd':
+                    sinceTimestamp = n * 24 * 60 * 60 * 1000;
+                    break;
+            }
+        } else if (/^\d+$/.test(since)) {
+            sinceTimestamp = parseInt(since) * 1000;
+        } else if (/^seq(\d+)$/.test(since)) {
+            sinceSequence = parseInt(since.substring(3));
+        }
+
+        {
+            let catchUp = [];
+            if (since === 'latest') {
+                catchUp.push(backlog[backlog.length - 1]);
+            } else {
+                catchUp = backlog.filter(message =>
+                    message.timestamp > (sinceTimestamp ?? 0) &&
+                    message.sequence > (sinceSequence ?? 0)
+                );
+            }
+
+            catchUp.forEach(msg => write(compose(msg)));
+        }
+    };
 
     this.send = msg => {
         const composed = compose(msg);
         clients.forEach(client => void client.write(composed));
+        wss.clients.forEach(ws => ws.ribbit.write(composed));
     };
 };
